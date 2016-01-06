ff17b2320d696eb50f2c0f74a95e264a9ba7efb4
Lars <hagen.lars.s@gmail.com>
2016-01-06 11:36:33 +0100

    Lagt til scripts som en test
    Hadde problemer med å committe til git, ingenting her skal fungere.
    Dette er bare en test for å se om commit virker.

---
+++ Steam_Buccaneers/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public static float forwardSpeed = 50;
+	public static int turnSpeed = 20;
+	public static int swingSpeed = 50;
+	public float rotationPerSecond = 15f;
+	public float rotationMax = 45f;
+//	public AudioSource wubWub;
+
+	// Use this for initialization
+	void Start () 
+	{
+//		AudioSource wubWub = GetComponent<AudioSource> ();
+//		wubWub.Stop ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		//må antakeligvis ha en timer for akkselerasjon/de
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			//AudioSource wubWub = GetComponent<AudioSource> ();
+			transform.Translate (Vector3.forward/forwardSpeed);
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.Rotate (Vector3.down, turnSpeed * Time.deltaTime);
+			//transform.Rotate (Vector3.up, swingSpeed * Time.deltaTime);
+			//endRotation.transform.Rotate(Vector3.left, 360, Space.World);	
+			//transform.Translate (Vector3.left/turnSpeed);
+			//	Quaternion 
+			//transform.rotation = Quaternion.AngleAxis
+			//transform.Rotate
+		}
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.Rotate (Vector3.up, turnSpeed * Time.deltaTime);
+			//transform.Rotate (Vector3.up, PlayerMovement.turnSpeed * Time.deltaTime);
+			//transform.Rotate (Vector3.down, swingSpeed * Time.deltaTime);
+			//endRotation.transform.Rotate(Vector3.right, 360, Space.World);
+			//transform.Translate (Vector3.right/turnSpeed);
+			//her blir det z og y +
+		}
+
+		//this.transform.rotation = Quaternion.Lerp(this.transform.rotation, endRotation.transform.rotation, Time.deltaTime*turnSpeed);
+	
+	}
+
+	//public void rotateShip (Vector3 axis, float angle
+}
---
+++ Steam_Buccaneers/Assets/Scripts/cameraControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraControl : MonoBehaviour 
+{
+	public float distanceAway;
+	private float amountScrolled;
+	public float scrollBy;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//scrollBy = 0.5;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// låser kamera til player
+		//Vector3 PlayerPOS = GameObject.Find("space donger 5 million").transform.transform.position;
+		Vector3 PlayerPOS = GameObject.Find("PlayerShip").transform.transform.position;
+		GameObject.Find("MainCamera").transform.position = new Vector3(PlayerPOS.x, (PlayerPOS.y)- distanceAway, (PlayerPOS.z)); 
+
+		// Kamera zoom
+		float scrollDistance = Input.GetAxisRaw("Mouse ScrollWheel");
+
+		if (scrollDistance > 0f)
+		{
+			// scroll in
+			if (distanceAway <= -1)
+			{
+				//Debug.Log (scrollDistance);
+				distanceAway += scrollBy;
+				//Debug.Log (distanceAway);
+			}
+		}
+		else if (scrollDistance < 0f)
+		{
+			// scroll out
+			if (distanceAway >= -4)
+			{
+				//Debug.Log (scrollDistance);
+				distanceAway -= scrollBy;
+				//Debug.Log (distanceAway);
+			}
+		}
+	}
+}
