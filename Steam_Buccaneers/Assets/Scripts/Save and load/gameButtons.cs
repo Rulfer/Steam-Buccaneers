d83ee3c8cd4b8f2fdb375078b314f6ebb0da9bea
Br4vO <john.haraldstad@hotmail.com>
2016-01-18 14:01:43 +0100

    Game UI
    Can open menu and load game and resume

---
+++ Steam_Buccaneers/Assets/Scripts/Save and load/gameButtons.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class gameButtons : MonoBehaviour {
+	private bool escMenuStatus = false;
+	public GameObject escMenu;
+
+	void Update()
+	{
+	if (Input.GetKeyDown (KeyCode.Escape))
+		{
+		setDifferent();
+		escMenu.SetActive(escMenuStatus);
+		}
+	}
+
+	public void resume()
+	{
+		escMenu.SetActive(!escMenuStatus);
+		setDifferent();
+	}
+
+	private void setDifferent()
+	{
+		escMenuStatus = !escMenuStatus;
+	}
+
+}
---
+++ Steam_Buccaneers/Assets/Scripts/Scene/onStartDeactivate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onStartDeactivate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.SetActive(false);
+	}
+}
