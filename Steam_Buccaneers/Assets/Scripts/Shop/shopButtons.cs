75ed5eabc4cdc04f0bbbb8c61d1245718f78f745
Br4vO <john.haraldstad@hotmail.com>
2016-01-26 14:20:37 +0100

    Fixed health regeneration
    Player can now not spend more money then they have.

--- Steam_Buccaneers/Assets/Scripts/Shop/shopButtons.cs
+++ Steam_Buccaneers/Assets/Scripts/Shop/shopButtons.cs
@@ -18,11 +18,16 @@ public class shopButtons : MonoBehaviour {
 
 	public void buyHealth()
 	{
-		if (GameControl.control.health < (int)GameObject.Find("Slider_refill").GetComponent<Slider>().value)
+		if (GameControl.control.health < (int)GameObject.Find("Slider_refill").GetComponent<Slider>().value && (int)GameObject.Find("value_cost_hp").GetComponent<updatePayment>().payment <= GameControl.control.money)
 		{
 			GameControl.control.health = (int)GameObject.Find("Slider_refill").GetComponent<Slider>().value;
 			GameObject.Find("Slider_current_hp").GetComponent<Slider>().value = GameControl.control.health;
 			GameControl.control.money -= (int)GameObject.Find("value_cost_hp").GetComponent<updatePayment>().payment;
+			GameObject.Find("value_scraps").GetComponent<Text>().text = GameControl.control.money.ToString();
 		}
+//		else if ((int)GameObject.Find("Slider_refill").GetComponent<Slider>().value < GameControl.control.money)
+//		{
+//			
+//		}
 	}
 }
