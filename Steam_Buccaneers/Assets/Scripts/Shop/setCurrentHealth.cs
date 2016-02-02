ad9c62351e4c2585f95005bd65c0d670f8d037cd
Br4vO <john.haraldstad@hotmail.com>
2016-02-02 14:03:54 +0100

    Fixed health buy again

---
+++ Steam_Buccaneers/Assets/Scripts/Shop/setCurrentHealth.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class setCurrentHealth : MonoBehaviour {
+	
+		void Start () 
+		{
+				gameObject.GetComponent<Slider>().value = GameControl.control.health;
+			}
+	}
