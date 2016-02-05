e473d412ec8eb1ac88f80f01d7af43390065d2e2
Bård Harald <lol_controll@hotmail.com>
2016-02-05 12:56:11 +0100

    Fikset stuff
    Vi har nå prefabs av AI og player. Også fikset noen scrips.

--- Steam_Buccaneers/Assets/Scripts/PlayerShip/PlayerRotate.cs
+++ Steam_Buccaneers/Assets/Scripts/PlayerShip/PlayerRotate.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class PlayerRotate : MonoBehaviour 
 {
-	public Vector3 uprightRotation = new Vector3 (90f,180f,0f);
-	Vector3 rotateShip = new Vector3 (0.0f, 0.2f, 0.0f);
+	public Vector3 uprightRotation = new Vector3 (0f,180f,0f);
+	Vector3 rotateShip = new Vector3 (0.0f, 0.0f, 0.2f);
 	public GameObject rotateThis;
 	// Use this for initialization
 	void Start () 
 	{
-		rotateThis = GameObject.Find("Small_Ship");
+		
 	}
 	
 	// Update is called once per frame
@@ -18,55 +18,55 @@ public class PlayerRotate : MonoBehaviour
 
 		if (PlayerMove2.turnLeft == true) 
 		{
-			if (rotateThis.transform.localEulerAngles.y < uprightRotation.y+20f)
+			if (rotateThis.transform.localEulerAngles.z < uprightRotation.z+20f)
 			{
-				//Debug.Log(rotateThis.transform.localEulerAngles.y + " this is rotation1");
+				//Debug.Log(rotateThis.transform.localEulerAngles.z + " this is rotation1");
 				rotateThis.transform.Rotate(transform.up, 5 * Time.deltaTime);
-				rotateThis.transform.localEulerAngles = new Vector3 (90f, 
-					rotateThis.transform.localEulerAngles.y, uprightRotation.z);
+				rotateThis.transform.localEulerAngles = new Vector3 (0f, 
+					rotateThis.transform.localEulerAngles.z, uprightRotation.z);
 			}
 		}
 
 			else
 		{
-			if (rotateThis.transform.localEulerAngles.y > uprightRotation.y)
+			if (rotateThis.transform.localEulerAngles.z > uprightRotation.z)
 			{
-				//Debug.Log(rotateThis.transform.localEulerAngles.y + " this is rotation2");
+				//Debug.Log(rotateThis.transform.localEulerAngles.z + " this is rotation2");
 				Vector3 rotateBack = rotateThis.transform.localEulerAngles - rotateShip;
 				rotateThis.transform.localEulerAngles = rotateBack;
-				//Debug.Log(rotateThis.transform.localEulerAngles.y + " this is rotation4");
+				//Debug.Log(rotateThis.transform.localEulerAngles.z + " this is rotation4");
 				//Debug.Log ("i hate you");
 				//
-				if (rotateThis.transform.localEulerAngles.y <= uprightRotation.y)
+				if (rotateThis.transform.localEulerAngles.z <= uprightRotation.z)
 				{
-					//Debug.Log(rotateThis.transform.localEulerAngles.y + " this is rotation3");
-					rotateThis.transform.localEulerAngles = new Vector3 (90f, 180f, 0f);
+					//Debug.Log(rotateThis.transform.localEulerAngles.z + " this is rotation3");
+					rotateThis.transform.localEulerAngles = new Vector3 (0f, 180f, 0f);
 				}
 			}
 		}
 
 		if (PlayerMove2.turnRight == true) 
 		{
-			if (rotateThis.transform.localEulerAngles.y > uprightRotation.y-20f)
+			if (rotateThis.transform.localEulerAngles.z > uprightRotation.z-20f)
 			{
 				rotateThis.transform.Rotate(-transform.up, 5 * Time.deltaTime);
-				rotateThis.transform.localEulerAngles = new Vector3 (90f, 
-					rotateThis.transform.localEulerAngles.y, uprightRotation.z);
+				rotateThis.transform.localEulerAngles = new Vector3 (0f, 
+					rotateThis.transform.localEulerAngles.z, uprightRotation.z);
 			}
 		}
 
 		else
 		{
-			if (rotateThis.transform.localEulerAngles.y < uprightRotation.y)
+			if (rotateThis.transform.localEulerAngles.z < uprightRotation.z)
 			{
 				Vector3 rotateBack = rotateThis.transform.localEulerAngles + rotateShip;
 				rotateThis.transform.localEulerAngles = rotateBack;
 				//rotateThis.transform.Rotate(transform.up,2f*Time.deltaTime);
 				//Debug.Log ("i hate you");
-				if (rotateThis.transform.localEulerAngles.y >= uprightRotation.y)
+				if (rotateThis.transform.localEulerAngles.z >= uprightRotation.z)
 				{
 					Debug.Log ("i hate you");
-					rotateThis.transform.localEulerAngles = new Vector3 (90f, 180f, 0f);
+					rotateThis.transform.localEulerAngles = new Vector3 (0f, 180f, 0f);
 				}
 			}
 		}
