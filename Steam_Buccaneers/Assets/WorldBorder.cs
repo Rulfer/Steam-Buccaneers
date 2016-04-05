996864702eaa8cbfb66a9724e418fe304378f69a
Rulfer <lol_controll@hotmail.com>
2016-04-05 15:29:40 +0200

    AI works in Tutorial again, border around the world
    I forgot to do a test to see if we are in the Tutorial scene, but this
    is now implemented. AI works there again.
    
    There is now a border going around the playable area, shown with the
    color red. When the player drives into this field a bool is set to true
    and a timer starts ticking down. This is displayed on the screen for the
    player to see. When the timer reaches 0, the player dies. Driving out of
    the red field resets the timer.

---
+++ Steam_Buccaneers/Assets/WorldBorder.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WorldBorder : MonoBehaviour 
+{
+	private bool isTrespassing = false;
+	private float killTimer = 0;
+	private float killDuration = 20;
+	public Text theText;
+	public Text numberText;
+
+	// Update is called once per frame
+	void Update () {
+		if(isTrespassing == true)
+		{
+			killDuration -= Time.deltaTime;
+			if(killTimer > killDuration)
+			{
+				GameControl.control.health = -1;
+			}
+			theText.text = "Return to the playable area!" + "\r\n";
+			theText.text += "You will die in       " + "     seconds.";
+			numberText.text = "\r\n" + "                                            " + (Mathf.Round(killDuration * 100f) / 100f).ToString();
+		}
+		else
+		{
+			killDuration = 20;
+			theText.text = "";
+			numberText.text = "";
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			isTrespassing = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			isTrespassing = false;
+		}
+	}
+}
