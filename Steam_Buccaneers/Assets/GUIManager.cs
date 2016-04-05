72bf3cef29b8bd817af1e00352e3752c0f23e391
Rulfer <lol_controll@hotmail.com>
2016-04-05 14:40:26 +0200

    New Minimap implemented!
    I made a new minimap from the bottom, and it is now implemented and
    fully functional! The issue we had with the previous one was that it
    would only display a minimap icon when the object was closer than X
    distance from the player, and display the WHOLE icon at the same time,
    not just what's inside the minimap. This was an issue espesially with
    big planets.
    
    The new minimap is round and simpler. Simpler, as in it shows everything
    that's inside the radius of the minimap. Not the entire icon, just
    what's inside of it.
    This is the guide I followed (there are 2 parts):
    https://www.youtube.com/watch?v=ZuV9Xlt-l6g

--- Steam_Buccaneers/Assets/GUIManager.cs
+++ Steam_Buccaneers/Assets/GUIManager.cs
@@ -5,17 +5,32 @@ public class GUIManager : MonoBehaviour {
 	public RenderTexture miniMapTexture;
 	public Material miniMapMaterial;
 
+	public Camera minimapCamera;
+
 	private float offset;
 
 	// Use this for initialization
 	void Awake () {
 		offset = 10;
 	}
+
+	public void zoomOut()
+	{
+		if(minimapCamera.orthographicSize < 500)
+			minimapCamera.orthographicSize += 50;
+	}
+
+	public void zoomIn()
+	{
+		if(minimapCamera.orthographicSize > 100)
+			minimapCamera.orthographicSize -= 50;
+	}
 	
 	// Update is called once per frame
 	void OnGUI () 
 	{
 		if(Event.current.type == EventType.Repaint)
-			Graphics.DrawTexture(new Rect(0, 0, 256, 256), miniMapTexture, miniMapMaterial);
+			//Graphics.DrawTexture(new Rect(0,0, 128, 128), miniMapTexture, miniMapMaterial);
+			Graphics.DrawTexture(new Rect(Screen.width / 1.13f, Screen.height / 9.7f, Screen.width / 9.5f, Screen.width / 9.5f), miniMapTexture, miniMapMaterial);
 	}
 }
