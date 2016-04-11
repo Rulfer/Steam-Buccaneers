20f0957d4fb97391275af09d20b04a08123aaca0
Lars <hagen.lars.s@gmail.com>
2016-04-11 19:51:50 +0200

    Treasure planet in the works
    what the title said

---
+++ Steam_Buccaneers/Assets/Scripts/World/OrbitMeteorsInner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitMeteorsInner : MonoBehaviour 
+{
+	Vector3 rotateLocalVec;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rotateLocalVec = new Vector3 (Mathf.RoundToInt(Random.Range(-1,1)), 
+			Mathf.RoundToInt(Random.Range(-1,1)),Mathf.RoundToInt(Random.Range(-1,1)));
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//this.transform.position = this.transform.parent.position;
+		this.transform.RotateAround (this.transform.parent.position, Vector3.up, .5f);
+		this.transform.Rotate(rotateLocalVec * Random.Range(.5f,1f));
+	}
+}
