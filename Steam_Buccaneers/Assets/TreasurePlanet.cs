bc0e2ab1eb63222a4e16cc7e8e631f6d7eefc527
Lars <hagen.lars.s@gmail.com>
2016-04-13 23:18:20 +0200

    Treasure Planet is well on its way
    Må rydde den opp litt, sette på at man faktisk dør av alle meteorer og
    slik, fikse collideren som får deg til å reise opp, animasjon til
    skattekista, osv.

--- Steam_Buccaneers/Assets/TreasurePlanet.cs
+++ Steam_Buccaneers/Assets/TreasurePlanet.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TreasurePlanet : MonoBehaviour 
@@ -14,6 +15,8 @@ public class TreasurePlanet : MonoBehaviour
 	float startTime;
 	bool niglet = false;
 	bool diglet = false;
+	bool pickUpTreasure = false;
+	bool pickingUpTreasure = false;
 
 
 	// Use this for initialization
@@ -28,11 +31,15 @@ public class TreasurePlanet : MonoBehaviour
 	{
 		if (niglet == true)
 		{
-			PickupTreasure();
+			GoUp();
 		}
 		if (diglet == true)
 		{
 			GoBackDown();
+		}
+		if (pickUpTreasure == true)
+		{
+			PickupTreasure();
 		}
 		//OnTriggerEnter();
 		//Debug.Log (enterPos + " EnterPosition");
@@ -42,7 +49,7 @@ public class TreasurePlanet : MonoBehaviour
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.tag == "Player" && treasureHasBeenPickedUp == false)
+		if (other.tag == "Player" && treasureHasBeenPickedUp == false && pickingUpTreasure == false)
 		{
 			//Debug.Log ("DO THIS PLEASE");
 			startTime = Time.time;
@@ -50,14 +57,15 @@ public class TreasurePlanet : MonoBehaviour
 			enterPos = player.transform.position;
 			enterFlyUp = new Vector3 (enterPos.x, enterPos.y + 80, enterPos.z);
 			travelLenght = Vector3.Distance(enterPos,enterFlyUp);
-
+			pickingUpTreasure = true;
 			niglet = true;
+			PlayerMove2.steerShip = false;
 		}
 			
 		//Destroy(player);
 	}
 
-	void PickupTreasure ()
+	void GoUp ()
 	{
 		//Debug.Log ("DO THIS PLEASE");
 
@@ -66,14 +74,45 @@ public class TreasurePlanet : MonoBehaviour
 		player.transform.position = Vector3.Lerp(enterPos,enterFlyUp,fracJourney);
 		if (player.transform.position.y >= 80)
 		{
-			diglet = true;
 			niglet = false;
-			treasureHasBeenPickedUp = true;
+			pickUpTreasure = true;
+
 			startTime = Time.time;
-			//treasureChest.GetComponent<Animation>;
-			//treasureChest.animation.Play();
 		}
 			
+	}
+
+	void PickupTreasure()
+	{
+		if (treasureHasBeenPickedUp != true)
+		{
+			float distCovered = (Time.time -startTime)*speed;
+			float fracJourney = distCovered /travelLenght;
+			player.transform.position = Vector3.Lerp(enterFlyUp,treasurePos,fracJourney);
+
+
+			if (player.transform.position == treasurePos)
+			{
+				//pickUpTreasure = false;
+				GameControl.control.money += 500;
+				treasureHasBeenPickedUp = true;
+				startTime = Time.time;
+				GameObject.Find("value_scraps_tab").GetComponent<Text>().text = GameControl.control.money.ToString();
+			}
+		}
+
+		else
+		{
+			float distCovered = (Time.time -startTime)*speed;
+			float fracJourney = distCovered /travelLenght;
+			player.transform.position = Vector3.Lerp(treasurePos,enterFlyUp,fracJourney);
+			if (player.transform.position == enterFlyUp)
+			{
+				startTime = Time.time;
+				pickUpTreasure = false;
+				diglet = true;
+			}
+		}
 	}
 
 	void GoBackDown()
@@ -84,10 +123,8 @@ public class TreasurePlanet : MonoBehaviour
 		player.transform.position = Vector3.Lerp(enterFlyUp,enterPos,fracJourney);
 		if (player.transform.position.y <= 0)
 		{
+			PlayerMove2.steerShip = true;
 			diglet = false;
-			//player.transform.position.y = 0F;
 		}
-
-
 	}
 }
