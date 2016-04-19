b7df2e454f13ef556d8bc72fd11dd7e2f66c8f54
Bård Harald <lol_controll@hotmail.com>
2016-04-19 13:26:31 +0200

    Background songs change based on where in the world the player is

--- Steam_Buccaneers/Assets/AudioController.cs
+++ Steam_Buccaneers/Assets/AudioController.cs
@@ -5,15 +5,22 @@ public class AudioController : MonoBehaviour
 {
 	private AudioSource backgroundSource;
 	private AudioSource combatSource;
+	public AudioClip[] clips;
 
 	private float counter = 0;
 	private float volumeCounter = 0;
 	private float resetCounter;
 
+	bool one = false;
+	bool two = false;
+	bool three = false;
+
 	// Use this for initialization
 	void Start ()
 	{
+		one = true;
 		backgroundSource = GameObject.Find("CameraChild").GetComponent<AudioSource>();
+		backgroundSource.clip = clips[0];
 		combatSource = GameObject.Find("CombatMusic").GetComponent<AudioSource>();
 		combatSource.volume = 0;
 		backgroundSource.volume = 1;
@@ -21,21 +28,29 @@ public class AudioController : MonoBehaviour
 
 	void Update()
 	{
-		if(SpawnAI.spawn.stopSpawn == true)
+		if(GameObject.Find("PlayerShip").transform.position.z < 4000)
 		{
-			if (combatSource.volume < 0.99f)
+			songOne();
+		}
+		if(GameObject.Find("PlayerShip").transform.position.z > 4200 && GameObject.Find("PlayerShip").transform.position.z < 11350)
+			songTwo();
+		if(GameObject.Find("PlayerShip").transform.position.z > 12000)
+			songThree();
+		if(SpawnAI.spawn.stopSpawn == true) //The spawning has topped, so a combat is ongoing
+		{
+			if (combatSource.volume < 0.99f) //The volume of the combat song is not 1 yet. 
 			{
-				if(!combatSource.isPlaying)
+				if(!combatSource.isPlaying) //If it's not playing, start playing the combat song
 					combatSource.Play();
-				volumeCounter += Time.deltaTime;
-				combatSource.volume = volumeCounter;
-				backgroundSource.volume = 1 - combatSource.volume;
-				if(volumeCounter >= 0.99f)
+				volumeCounter += Time.deltaTime; //Increase the volume. After 1 second the volume will be 1. 
+				combatSource.volume = volumeCounter; //Sets the volum
+				backgroundSource.volume = 1 - combatSource.volume; //Decrese background song based on combat song volume
+				if(volumeCounter >= 1) //The combat volume has reached 1
 				{
-					counter = 0;
-					volumeCounter = 0;
-					backgroundSource.volume = 0;
-					backgroundSource.Stop();
+					counter = 0; //Reset the counter
+					volumeCounter = 0; //Reset the conter
+					backgroundSource.volume = 0; //Set the volume of background source to 0
+					backgroundSource.Stop(); //Stop the background source
 				}
 			}
 		}
@@ -67,8 +82,88 @@ public class AudioController : MonoBehaviour
 				}
 			}
 		}
+	}
 
-		Debug.Log("combatSource.volume = " + combatSource.volume);
+	private void songOne()
+	{
+		if(!one)
+		{
+			backgroundSource.volume -= Time.deltaTime;
+			if(backgroundSource.volume <= 0)
+			{
+				backgroundSource.clip = clips[0];
+				backgroundSource.Play();
 
+				one = true;
+				two = false;
+				three = false;
+			}
+		}
+		else
+		{
+			if(backgroundSource.volume < 1)
+			{
+				backgroundSource.volume += Time.deltaTime;
+			}
+			else
+			{
+				backgroundSource.volume = 1;
+			}
+		}
+	}
+
+	private void songTwo()
+	{
+		if(!two)
+		{
+			backgroundSource.volume -= Time.deltaTime;
+			if(backgroundSource.volume <= 0)
+			{
+				backgroundSource.clip = clips[1];
+				backgroundSource.Play();
+				two = true;
+				one = false;
+				three = false;
+			}
+		}
+		else
+		{
+			if(backgroundSource.volume < 1)
+			{
+				backgroundSource.volume += Time.deltaTime;
+			}
+			else
+			{
+				backgroundSource.volume = 1;
+			}
+		}
+	}
+
+	private void songThree()
+	{
+		if(!three)
+		{
+			backgroundSource.volume -= Time.deltaTime;
+			if(backgroundSource.volume <= 0)
+			{
+				backgroundSource.clip = clips[2];
+				backgroundSource.Play();
+
+				three = true;
+				one = false;
+				two = false;
+			}
+		}
+		else
+		{
+			if(backgroundSource.volume < 1)
+			{
+				backgroundSource.volume += Time.deltaTime;
+			}
+			else
+			{
+				backgroundSource.volume = 1;
+			}
+		}
 	}
 }
