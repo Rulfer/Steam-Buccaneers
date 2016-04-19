4db3e5e5e50834ee098ab2b338ef8043ddfa5cdb
Br4vO <john.haraldstad@hotmail.com>
2016-04-19 12:59:02 +0200

    Small cleanup

---
+++ Steam_Buccaneers/Assets/Scripts/World/OpenCloseObjectives.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenCloseObjectives : MonoBehaviour {
+
+	public GameObject objectMenu;
+	
+	public void openCloseObjectivemenu()
+	{
+		objectMenu.SetActive (!objectMenu.activeSelf);
+	}
+
+}
