2a520072b3a95e549131f9acaf3b38752b8fe5c6
Br4vO <john.haraldstad@hotmail.com>
2016-04-21 18:33:06 +0200

    Animert sol som lagger systemet

---
+++ Steam_Buccaneers/Assets/animateSun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animateSun : MonoBehaviour {
+
+	public Material[] sunTextures = new Material[71];
+	private float temp;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		temp += Time.deltaTime*24;
+		this.gameObject.GetComponent<MeshRenderer> ().material = new Material(sunTextures[Mathf.RoundToInt(temp)]);
+		Debug.Log (this.gameObject.GetComponent<MeshRenderer> ().material);
+		Debug.Log (temp);
+
+		if (temp >= 71)
+			temp = 0;
+	}
+}
