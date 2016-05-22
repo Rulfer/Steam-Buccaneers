30c76710b795526983f5b3b0242f87ed30bf449c
Bård Harald <lol_controll@hotmail.com>
2016-05-22 17:42:20 +0200

    BOSS FINALLY DONT GET TRIGGERED BY BOMBS, updated DeadPlayer

--- Steam_Buccaneers/Assets/Scripts/PlayerShip/DeadPlayer.cs
+++ Steam_Buccaneers/Assets/Scripts/PlayerShip/DeadPlayer.cs
@@ -3,45 +3,32 @@ using System.Collections;
 
 public class DeadPlayer : MonoBehaviour 
 {
-	public GameObject boom;
-	public Vector3 axisOfRotation;
-	public float angularVelocity;
+	public GameObject boom; //Reference to the explosion simulation
+	public Vector3 axisOfRotation; //Random rotation axis
+	public float angularVelocity; //Random velocity for the rotation
 
-	private float rotateTimer = 0;
-	private float rotateDuration = 5;
+	private float rotateTimer = 0; //Used to stop the timer
+	private float rotateDuration = 5; //How long it takes for the ship to reach a standstill
 
-	private AudioSource source;
-	public AudioClip[] clips;
-
-//	// Use this for initialization
-//	void Start () 
-//	{
-//		axisOfRotation = Random.onUnitSphere;
-//		angularVelocity = Random.Range (20, 40);
-//		this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-//		Instantiate(boom, this.transform.position, this.transform.rotation);
-//		source = this.GetComponent<AudioSource>();
-//		source.clip = clips[Random.Range(0, 5)];
-//		source.volume = 1;
-//		source.Play();
-//	}
+	private AudioSource source; //Audiosource
+	public AudioClip[] clips; //Array of audio
 
 	public void killPlayer()
 	{
-		axisOfRotation = Random.onUnitSphere;
-		angularVelocity = Random.Range (20, 40);
-		this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-		Instantiate(boom, this.transform.position, this.transform.rotation);
-		source = this.GetComponent<AudioSource>();
-		source.clip = clips[Random.Range(0, 5)];
-		source.volume = 1;
-		source.Play();
+		axisOfRotation = Random.onUnitSphere; //Random rotation direction
+		angularVelocity = Random.Range (20, 40); //Random velocity
+		this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; //Remove constraints to allow other ships to push it
+		Instantiate(boom, this.transform.position, this.transform.rotation); //Instantiate the particle simulation at this position
+		source = this.GetComponent<AudioSource>(); //Get the audio source
+		source.clip = clips[Random.Range(0, 5)]; //Play a random audio clip
+		source.volume = 1; //Set the volume to 1
+		source.Play(); //Play the clip
 	}
 
 	void Update()
 	{
-		rotateDuration -= Time.deltaTime;
-		if(rotateDuration > rotateTimer)
+		rotateDuration -= Time.deltaTime; //Time has passed
+		if(rotateDuration > rotateTimer) //Still not rotated enough
 			this.transform.Rotate(axisOfRotation, angularVelocity * Time.smoothDeltaTime * rotateDuration * 0.5f); //Rotates the object
 	}
 }
