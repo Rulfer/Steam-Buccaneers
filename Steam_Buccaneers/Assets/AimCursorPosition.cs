9f5eee9414a0ac75cef86864e10550a4ee5dcc06
Bård Harald <lol_controll@hotmail.com>
2016-05-23 12:27:56 +0200

    cries in a corner

--- Steam_Buccaneers/Assets/AimCursorPosition.cs
+++ Steam_Buccaneers/Assets/AimCursorPosition.cs
@@ -7,11 +7,15 @@ public class AimCursorPosition : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		this.GetComponent<RawImage>().enabled = false;
+		this.GetComponent<RawImage>().enabled = false; //Disables the aimed cursor
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = Input.mousePosition;
+		//Constantly update the position of the aimed cursor to be that of the main cursor. 
+		//This is to prevent the player from seeing the cursor being visible and take a jump from
+		//yhe previous position to the new position, and instead give them a smoother experience
+		//with the cursor appearing instantly at the correct position. 
+		this.transform.position = Input.mousePosition; 
 	}
 }
